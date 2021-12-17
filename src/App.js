67f288f70725c66669bcbe93731bf93e3af2a917
Danylo Santoro <dsantoro@gmail.com>
2021-12-17 13:56:48 +0000

    adicionar imagens

--- src/App.js
+++ src/App.js
@@ -5,34 +5,57 @@ const products = [
   {
     id: 1,
     title: "Headset Beats EP com fio",
-    image: "https://flowbite.com/docs/images/blog/image-1.jpg",
+    image: require("./assets/headset.jpeg"),
     description:
       "Com algumas marcas de uso mas super bem cuidado. Tem o fio roído mas funciona legal. / With some marks of use but super well taken care of. Has the thread chewed but works fine.",
     slug: "Headset Beats EP",
+    price: "40 euros",
+    originalLink:
+      "https://www.worten.pt/tv-video-e-som/som-portatil/auscultadores-e-auriculares/auscultadores/auscultadores-com-fio-beats-ep-on-ear-microfone-preto-5951411",
   },
   {
     id: 2,
     title: "Teclado Mecânico TKL Kernel",
-    image: "https://flowbite.com/docs/images/blog/image-1.jpg",
+    image: require("./assets/keyboard.jpeg"),
     description:
       "Teclado mecânico de alta qualidade. Com apoio em alumínio, luzes configuráveis e bem pouco uso. / High quality mechanical keyboard. With aluminum backing, configurable lights and very little use.",
     slug: "Teclado Mecânico TKL Kernel",
+    price: "30 euros",
+    originalLink:
+      "https://www.worten.pt/gaming/acessorios-gaming-pc/teclados-gaming/teclado-gaming-krom-kernel-tkl-mecanico-layout-portugues-switch-red-outemu-iluminado-6882499",
   },
   {
     id: 3,
     title: "Adaptador USB-C pra 8 saídas diversas",
-    image: "https://flowbite.com/docs/images/blog/image-1.jpg",
+    image: require("./assets/adaptor.jpeg"),
     description:
       "Usado apenas uma vez. Possibilita o uso de 3 monitores mas apenas em Windows e Linux. / Used only once. Allows the use of 3 monitors but only on Windows and Linux.",
     slug: "Adaptador USB-C pra 8 saídas diversas",
+    price: "40 euros",
+    originalLink:
+      "https://www.amazon.es/gp/product/B09FXLZ1L3/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1",
   },
   {
     id: 4,
     title: "Suporte de Monitor",
-    image: "https://flowbite.com/docs/images/blog/image-1.jpg",
+    image: require("./assets/support.jpeg"),
     description:
       "Produto novo, nunca usado. Super robusto e flexível. / New product, never used. Super robust and flexible.",
-    slug: "Item",
+    slug: "Suporte de Monitor",
+    price: "30 euros",
+    originalLink:
+      "https://www.pccomponentes.pt/equip-soporte-de-mesa-inclinable-para-monitor-13-27-vesa-100x100-max-65kg",
+  },
+  {
+    id: 5,
+    title: "Monitor ASUS VZ239HE (23'' - Full HD - LED IPS)",
+    image: require("./assets/monitor.jpeg"),
+    description:
+      "Pouco usado. Funciona muito bem e está na caixa. / Pouco usado. Funciona muito bem e está na caixa.",
+    slug: "Monitor ASUS VZ239HE",
+    price: "70 euros",
+    originalLink:
+      "https://www.worten.pt/informatica-e-acessorios/monitores/monitores-pc/monitor-asus-vz239he-23-full-hd-led-ips-6418351",
   },
 ];
 
@@ -53,7 +76,9 @@ function App() {
                 title={product.title}
                 description={product.description}
                 image={product.image}
+                price={product.price}
                 slug={product.slug}
+                originalLink={product.originalLink}
               />
             );
           })}
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,29 @@
-const Card = ({ image, title, description, slug }) => {
+const Card = ({ image, title, description, price, originalLink, slug }) => {
   return (
     <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
       <img className="rounded-t-lg" src={image} alt={title} />
       <div className="p-5">
-        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 h-16">
+        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 h-16 overflow-hidden">
           {title}
         </h5>
-        <p className="font- text-sm text-gray-700 mb-3 h-36">{description}</p>
+        <p className="font- text-sm text-gray-700 mb-3 h-32">{description}</p>
+        <p className="text-xl font-bold mb-4">{price}</p>
         <a
-          className="text-white bg-blue-300 hover:bg-blue-800 ffont-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center"
+          className="text-white bg-blue-300 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center mb-4"
           href={`https://api.whatsapp.com/send?phone=+351910272254&text=${slug}`}
           target="_blank"
           rel="noreferrer"
         >
           Tenho interesse / I want it
+        </a>
+        <hr className="mb-2" />
+        <a
+          className="text-sm text-gray-600"
+          href={originalLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Link do produto na loja / <small>Link of product in store</small>
         </a>
       </div>
     </div>
