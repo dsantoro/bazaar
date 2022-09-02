9b155d200e271b0d4b090a1701d1eee7ebb2fea9
Danylo Santoro <danylo.santoro@celfocus.com>
2022-09-02 14:04:51 +0100

    more itens

--- src/App.js
+++ src/App.js
@@ -25,17 +25,6 @@ const products = [
       "https://www.worten.pt/gaming/acessorios-gaming-pc/teclados-gaming/teclado-gaming-krom-kernel-tkl-mecanico-layout-portugues-switch-red-outemu-iluminado-6882499",
   },
   {
-    id: 3,
-    title: "Adaptador USB-C pra 8 saídas diversas",
-    image: require("./assets/adaptor.jpeg"),
-    description:
-      "Usado apenas uma vez. Possibilita o uso de 3 monitores mas apenas em Windows e Linux. / Used only once. Allows the use of 3 monitors but only on Windows and Linux.",
-    slug: "Adaptador USB-C pra 8 saídas diversas",
-    price: "40 euros",
-    originalLink:
-      "https://www.amazon.es/gp/product/B09FXLZ1L3/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1",
-  },
-  {
     id: 4,
     title: "Suporte de Monitor",
     image: require("./assets/support.jpeg"),
@@ -48,14 +37,23 @@ const products = [
   },
   {
     id: 5,
-    title: "Monitor ASUS VZ239HE (23'' - Full HD - LED IPS)",
-    image: require("./assets/monitor.jpeg"),
-    description:
-      "Pouco usado. Funciona muito bem e está na caixa. / Pouco usado. Funciona muito bem e está na caixa.",
-    slug: "Monitor ASUS VZ239HE",
-    price: "70 euros",
+    title: "Microfone Trust GXT 232 Mantis Streaming",
+    image: require("./assets/microfone.jpeg"),
+    description: "Pouco uso e muito boa qualidade.",
+    slug: "Microfone Trust GXT 232 Mantis Streaming",
+    price: "15 euros",
     originalLink:
-      "https://www.worten.pt/informatica-e-acessorios/monitores/monitores-pc/monitor-asus-vz239he-23-full-hd-led-ips-6418351",
+      "https://www.worten.pt/informatica-e-acessorios/acessorios-pc/mais-acessorios-pc/microfones/microfone-trust-gxt-232-mantis-streaming-com-cabo-preto-6725198?gclid=Cj0KCQjw08aYBhDlARIsAA_gb0ehEBc143stNd9aJrW4kCTe3xjM0frwIoWOoU217vS3ukDr3Pb9eEEaAt2gEALw_wcB",
+  },
+  {
+    id: 6,
+    title: "Auscultadores Bluetooth JBL T510 (Over Ear - Microfone - Preto)",
+    image: require("./assets/fonejbl.jpeg"),
+    description: "Menos de um mes de uso. Sem fio",
+    slug: "Auscultadores Bluetooth JBL T510 (Over Ear - Microfone - Preto)",
+    price: "25 euros",
+    originalLink:
+      "https://www.worten.pt/tv-video-e-som/som-portatil/auscultadores/auscultadores-sem-fios/auscultadores-bluetooth-jbl-t510-over-ear-microfone-preto-7374086",
   },
 ];
 
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,9 @@
 const Card = ({ image, title, description, price, originalLink, slug }) => {
   return (
     <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm mb-5">
-      <img className="rounded-t-lg" src={image} alt={title} />
+      <picture className="rounded-t-lg image-square">
+        <img src={image} alt={title} />
+      </picture>
       <div className="p-5">
         <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 h-16 overflow-hidden">
           {title}
